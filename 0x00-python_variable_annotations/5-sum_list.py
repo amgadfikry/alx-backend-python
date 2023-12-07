744158369f922752b08d8cf50bc71aeec26c4c49
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-12-07 17:22:43 +0200

    Write a type-annotated function sum_list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 """ type-annotated function """
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """ sum_list - takes a list of floats as argument and returns their sum
     Args:
