60c613a0c6387311871711eb784214369e03bf0f
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-12-07 17:35:31 +0200

    Write a type-annotated function sum_mixed_list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,10 +4,10 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """ sum_mixed_list - takes a list of floats as argument and returns their sum
+    """ sum_mixed_list - takes a list mxd_lst of floats and ints
     Args:
-        mxd_lst (list[Union[int, float]]): list of floats
+        mxd_lst (List[Union[int, float]]): list of floats and ints
     Returns:
-        float: sum of floats
+        float: sum of floats and ints
     """
     return sum(mxd_lst)
