10e9cc561fd4568e30cda7c0fce5bd33f0cb2982
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-12-11 16:12:51 +0200

    use async function multiple times

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,6 +5,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
-    li = [wait_random(max_delay) for _ in range(0, n)]
-    waiting = await asyncio.gather(*li)
-    return sorted(waiting)
+    '''Executes wait_random n times.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
