92fab55ce63226afe4c668b72665924794362746
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-12-12 11:31:11 +0200

    loopover await and async

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,8 +5,8 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ generate a generator """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
