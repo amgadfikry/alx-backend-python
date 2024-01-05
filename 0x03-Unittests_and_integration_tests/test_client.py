80aa32997ff8edad891efecff6affe359b689438
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-05 21:18:59 +0200

    add test cases for org method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,31 +2,26 @@
 """ unittest of client module functions """
 import unittest
 from typing import Dict
-from unittest.mock import patch, Mock
+from unittest.mock import patch, MagicMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """ class to test client methods
-        inside class
-    """
+    """Tests the `GithubOrgClient` class."""
     @parameterized.expand([
         ('google', {'load': 'google'}),
         ('abc', {'load': 'abc'})
     ])
     @patch('client.get_json')
-    def test_org(
-            self, name: str, load: Dict, mock_get: Mock) -> None:
-        """ method that test org method in GithubOrgClient class """
-        mock_res: Mock = Mock()
-        mock_res.return_value = load
-        mock_get.return_value = mock_res
-        call_class: GithubOrgClient = GithubOrgClient(name)
-        result: Dict = call_class.org()
-        mock_get.assert_called_once_with(call_class.ORG_URL.format(org=name))
-        mock_res.assert_called_once()
-        self.assertEqual(result, load)
+    def test_org(self, name: str, load: Dict, mock_get: MagicMock) -> None:
+        """Tests the `org` method."""
+        mock_get.return_value = MagicMock(return_value=load)
+        gh_org_client = GithubOrgClient(name)
+        self.assertEqual(gh_org_client.org(), load)
+        mock_get.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(name)
+        )
 
 
 if __name__ == '__main__':
