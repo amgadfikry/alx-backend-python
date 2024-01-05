35030c6f570c859bb7990531860f58b3e71d5034
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-05 21:36:50 +0200

    add new test for test public repo url property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """ unittest of client module functions """
 import unittest
 from typing import Dict
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -27,6 +27,14 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get.assert_called_once_with(call_class.ORG_URL.format(org=name))
         mock_res.assert_called_once()
         self.assertEqual(result, load)
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_prop: PropertyMock):
+        """ test method public repo url"""
+        mock_prop.return_value = {'repos_url': 'pla'}
+        call_class: GithubOrgClient = GithubOrgClient("pla")
+        result: str = call_class._public_repos_url
+        self.assertEqual(result, 'pla')
 
 
 if __name__ == '__main__':
