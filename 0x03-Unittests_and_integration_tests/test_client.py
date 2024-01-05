8de982006fc650783f476bb81f573e5ecf1f5103
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-05 23:03:35 +0200

    try another way to test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -34,13 +34,13 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_public_repos(self, mock_get: Mock) -> None:
         """ test public repos attribute"""
-        mock_get.return_value = [{"name": 'a'}]
-        test_class = GithubOrgClient("test_org")
-        with patch.object(test_class, "_public_repos_url", 'pla'):
-            results = test_class.public_repos()
-            self.assertEqual(results, ['a'])
-            mock_get.assert_called_once_with('pla')
-            self.assertEqual(test_class._public_repos_url, 'pla')
+        test_class: GithubOrgClient = GithubOrgClient('pla')
+        mock_get.return_value: Dict = [{'name': 'a', 'license': None}]
+        with patch('client.GithubOrgClient._public_repos_url') as mock_url:
+            mock_url.return_value: str = 'pla'
+            result: List = test_class.public_repos()
+            mock_get.assert_called_once_with(mock_url)
+        self.assertEqual(result, ['a'])
 
 
 if __name__ == '__main__':
