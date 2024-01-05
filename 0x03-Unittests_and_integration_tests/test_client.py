366ab061e329c0d365f0ecea160666d76bcf7142
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-05 22:41:36 +0200

    create test for pubic repo method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -35,12 +35,11 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_public_repos(self, mock_get: Mock) -> None:
         """ test public repos attribute"""
         test_class: GithubOrgClient = GithubOrgClient('pla')
-        mock_get.return_value: Dict = [{'name': 'a', 'license': None}]
-        with patch('client.GithubOrgClient._public_repos_url') as mock_url:
-            mock_url.return_value: str = 'pla'
+        mock_get.return_value: List = [{'name': 'a', 'license': None}]
+        with patch('client.GithubOrgClient._public_repos_url', return_value='pla') as mock_url:
             result: List = test_class.public_repos()
             mock_get.assert_called_once_with(mock_url)
-        self.assertEqual(result, ['a'])
+            self.assertEqual(result, ['a'])
 
 
 if __name__ == '__main__':
