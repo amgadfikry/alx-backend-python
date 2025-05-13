9aca0257fa3ea67dd358d99ee8f7632368d8d12d
amgadfikry <dr.amgad_sh92@yahoo.com>
2025-05-13 19:57:59 +0300

    add stream generator to calulate age cagerage

---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+seed = __import__('seed')
+
+
+def stream_user_ages():
+    """Generator to yield user ages one by one
+    Yields:
+        int: Age of each user.
+    """
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor()
+    cursor.execute("SELECT age FROM user_data") 
+    for row in cursor:
+        yield row[0] 
+    connection.close()
+
+
+def calculate_average_age():
+    """Calculates the average age using the generator and prints it.
+    Rturns:
+        None
+    """
+    total_age = 0
+    count = 0
+    for age in stream_user_ages(): 
+        total_age += age
+        count += 1
+    print(f"Average age of users: {total_age / count}")
+
+calculate_average_age()
