ad2313b7a893f86f4b3708c4b06a9ba155133f69
amgadfikry <dr.amgad_sh92@yahoo.com>
2025-05-23 16:30:33 +0300

    add context class

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None:
+            self.conn.rollback()
+        else:
+            self.conn.commit()
+        self.conn.close()
+
+### Usage of the context manager
+with DatabaseConnection('test') as conn:
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    results = cursor.fetchall()
