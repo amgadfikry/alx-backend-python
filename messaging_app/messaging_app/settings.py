45e1021ea3e012f6c359f22eebe9b3f1f9bbad3d
amgadfikry <dr.amgad_sh92@yahoo.com>
2025-06-01 15:00:51 +0300

    initial commit

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -39,6 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
